1bcad62e8bf2ba485de0b712c0d37ded216528c9
Iulian Caluian <iulian.coder@gmail.com>
2023-01-30 01:41:12 +0200

    Refactoring barrier move in a VM

--- ioDeviceEmulator/Client/Models/Barrier.cs
+++ ioDeviceEmulator/Client/Models/Barrier.cs
@@ -8,27 +8,9 @@ namespace ioDeviceEmulator.Client.Models
 {
     public class Barrier
     {
-        private int _degree = 0;
-        private readonly int _autoClosingTime;
-        private readonly int _sppedOfOneDegMoveMs;
-        private readonly object _lock = new object();
-        private CancellationTokenSource _cts = new CancellationTokenSource();
 
-        public int Degree
-        {
-            get { return _degree; }
-            set
-            {
-                lock (_lock)
-                {
-                    if (value >= 0 && value <= 90)
-                    {
-                        _degree = value;
-                    }
-                }
-            }
-        }
-
+         private readonly int _autoClosingTime;
+ 
 
         // Control:
         public BarrierTerminal Start { get; set; }
@@ -44,16 +26,14 @@ namespace ioDeviceEmulator.Client.Models
         public BarrierTerminal LoopDetector { get; set; }
 
 
-        public Barrier(int autoClosingTimeInMs, int sppedOfOneDegMoveMs)
+        public Barrier(int autoClosingTimeInMs)
         {
             _autoClosingTime = autoClosingTimeInMs;
-            _sppedOfOneDegMoveMs = sppedOfOneDegMoveMs;
 
             Start = new BarrierTerminal();
             Start.PropertyChanged += Start_PropertyChanged;
 
             Stop = new BarrierTerminal();
-            Stop.PropertyChanged += Stop_PropertyChanged;
             Start2 = new BarrierTerminal();
             
             PhotoCell = new BarrierTerminal();
@@ -62,91 +42,33 @@ namespace ioDeviceEmulator.Client.Models
 
         private void Start_PropertyChanged(object? sender, PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == "Activated")
-            {
-                var myObject = sender as BarrierTerminal;
-                if (myObject != null)
-                {
-                    if (myObject.Activated)
-                    {
-                        Task.Run(() => Open());
-                    } 
-                    else
-                    {
-                        Task.Run(() => AutomaticClose());
-                    }
+            //if (e.PropertyName == "Activated")
+            //{
+            //    var myObject = sender as BarrierTerminal;
+            //    if (myObject != null)
+            //    {
+            //        if (myObject.Activated)
+            //        {
+            //            Task.Run(() => Open());
+            //        } 
+            //        else
+            //        {
+            //            Task.Run(() => AutomaticClose());
+            //        }
                     
-                }
-            }
+            //    }
+            //}
         }
 
-        private void Stop_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "Activated")
-            {
-                var myObject = sender as BarrierTerminal;
-                if (myObject != null)
-                {
-                    if (myObject.Activated)
-                    {
-                        Task.Run(() => Close());
-                    }
-                    else
-                    {
-                        // Nothing
-                    }
 
-                }
-            }
-        }
 
         // Logic:
 
 
-        private async Task Open()
-        {
-            lock (_lock)
-            {
-                _cts.Cancel();
-                _cts = new CancellationTokenSource();
-            }
+  
 
-            for (int i = Degree; i <= 90; i++)
-            {
-                Debug.WriteLine("Here is " + i + " t:" + DateTime.UtcNow.ToString("fff"));
-                Degree = i;
-                await Task.Delay(_sppedOfOneDegMoveMs, _cts.Token);
-                Debug.WriteLine("Here is out " + i + " t:" + DateTime.UtcNow.ToString("fff"));
-            }
-        }
 
-        private async Task Close()
-        {
-            lock (_lock)
-            {
-                _cts.Cancel();
-                _cts = new CancellationTokenSource();
-            }
-
-            for (int i = Degree; i >= 0; i--)
-            {
-                Degree = i;
-                await Task.Delay(_sppedOfOneDegMoveMs, _cts.Token);
-            }
-        }
-
-        private async Task AutomaticClose()
-        {
-            lock (_lock)
-            {
-                _cts.Cancel();
-                _cts = new CancellationTokenSource();
-            }
-
-            await Task.Delay(_autoClosingTime, _cts.Token);
-            
-            await Close();
-        }
+        
 
     }
 }
---
+++ ioDeviceEmulator/Client/ViewModels/BarrierViewModel.cs
@@ -0,0 +1,81 @@
+﻿namespace ioDeviceEmulator.Client.ViewModels
+{
+    public class BarrierViewModel
+    {
+  
+        private readonly object _lock = new object();
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+
+        private int _rotation = 0;
+
+        public event EventHandler<EventArgs> RotationChanged;
+
+        public int Rotation
+        {
+            get { return _rotation; }
+            set
+            {
+                lock (_lock)
+                {
+                    if (value >= 0 && value <= 90)
+                    {
+                        _rotation = value;
+                    }
+                }
+            }
+        }
+
+        public  async Task OpenBarrier()
+        {
+            CancellationTokenSource cts;
+            lock (_lock)
+            {
+                _cts.Cancel();
+                _cts = cts = new CancellationTokenSource();
+            }
+
+            while (Rotation <= 90)
+            {
+                if (cts.IsCancellationRequested)
+                    break;
+
+                Rotation++;
+                await Task.Delay(5, cts.Token);
+                OnRotationChanged();
+            }
+
+        }
+
+        private void OnRotationChanged()
+        {
+            RotationChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public async Task CloseBarrier()
+        {
+            CancellationTokenSource cts;
+            lock (_lock)
+            {
+                _cts.Cancel();
+                _cts = cts = new CancellationTokenSource();
+            }
+
+            while (Rotation >= 0)
+            {
+                if (cts.IsCancellationRequested)
+                    break;
+
+                Rotation--;
+                await Task.Delay(5, cts.Token);
+                OnRotationChanged();
+            }
+
+        }
+
+        public Task PauseBarrier()
+        {
+            _cts.Cancel();
+            return Task.CompletedTask;
+        }
+    }
+}
